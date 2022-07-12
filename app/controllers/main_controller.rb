269e06b0833625f0933648fc9d65cdfcc702731d
Navid Seyedashrafi <navidalexander@Navids-MacBook-Pro.local>
2022-07-11 17:16:40 -0700

    Created a Category Model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def home
+    @categories = Category.all
   end
 
   def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Category.create(name: "Sports")
+Category.create(name: "Politics")
+Category.create(name: "Local")
+Category.create(name: "National")
+Category.create(name: "Global")
